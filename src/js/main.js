03b5b680f47389f7f29eda8aefa5b354ab44e610
Abdullah Munshi <stackbind@gmail.com>
2023-10-15 18:39:02 +0600

    book appointment button added and it's scroll on click

--- src/js/main.js
+++ src/js/main.js
@@ -14,3 +14,26 @@ const players = Array.from(document.querySelectorAll(".video_player")).map(
       ratio: "16:9",
     })
 );
+
+// Header background change
+function changeHeader() {
+  const header = document.querySelector("header");
+  if (window.scrollY > 5) {
+    header.classList.add("change");
+  } else {
+    header.classList.remove("change");
+  }
+}
+window.addEventListener("scroll", changeHeader);
+
+// Scroll to a tergeted section
+const scrollToSectionButton = document.querySelectorAll(
+  "[href='#calendlyArea']"
+);
+const targetSection = document.getElementById("calendlyArea");
+scrollToSectionButton.forEach((each) => {
+  each.addEventListener("click", (e) => {
+    e.preventDefault();
+    targetSection.scrollIntoView({ behavior: "smooth" });
+  });
+});
