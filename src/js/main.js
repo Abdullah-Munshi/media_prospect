475f823b8ce1e4bce1f3700f6d9ffc608a49eb78
Abdullah Munshi <stackbind@gmail.com>
2023-10-15 18:06:08 +0600

    preloader hide based on heroPlayer event

--- src/js/main.js
+++ src/js/main.js
@@ -4,14 +4,13 @@ const heroPlayer = new Plyr("#heroPlayer", {
   ratio: "16:9",
 });
 
+heroPlayer.on("ready", function () {
+  document.getElementById("preloader").classList.add("hide");
+});
+
 const players = Array.from(document.querySelectorAll(".video_player")).map(
   (p) =>
     new Plyr(p, {
       ratio: "16:9",
     })
 );
-
-// Hide the preloader when the page is fully loaded
-document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("preloader").classList.add("hide");
-});
