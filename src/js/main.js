6271cc9c77c968d0cc9783018be4dfd0e89682ba
Abdullah Munshi <stackbind@gmail.com>
2023-10-16 02:57:41 +0600

    client update done need some makeup

--- src/js/main.js
+++ src/js/main.js
@@ -1,39 +1,6 @@
-import Plyr from "plyr";
-
-const heroPlayer = new Plyr("#heroPlayer", {
-  ratio: "16:9",
-});
-
-heroPlayer.on("ready", function () {
-  document.getElementById("preloader").classList.add("hide");
-});
-
-const players = Array.from(document.querySelectorAll(".video_player")).map(
-  (p) =>
-    new Plyr(p, {
-      ratio: "16:9",
-    })
-);
-
-// Header background change
-function changeHeader() {
-  const header = document.querySelector("header");
-  if (window.scrollY > 5) {
-    header.classList.add("change");
-  } else {
-    header.classList.remove("change");
-  }
-}
-window.addEventListener("scroll", changeHeader);
-
-// Scroll to a tergeted section
-const scrollToSectionButton = document.querySelectorAll(
-  "[href='#calendlyArea']"
-);
-const targetSection = document.getElementById("calendlyArea");
-scrollToSectionButton.forEach((each) => {
-  each.addEventListener("click", (e) => {
-    e.preventDefault();
-    targetSection.scrollIntoView({ behavior: "smooth" });
-  });
+import pageTwicks from "./pageTwicks";
+import setupSlider from "./slider-config";
+document.addEventListener("DOMContentLoaded", function () {
+  pageTwicks();
+  setupSlider();
 });
---
+++ src/js/pageTwicks.js
@@ -0,0 +1,31 @@
+import { heroPlayer } from "./videoPlayer";
+function pageTwicks() {
+  heroPlayer.on("ready", function () {
+    document.getElementById("preloader").classList.add("hide");
+  });
+
+  // Header background change
+  function changeHeader() {
+    const header = document.querySelector("header");
+    if (window.scrollY > 5) {
+      header.classList.add("change");
+    } else {
+      header.classList.remove("change");
+    }
+  }
+  window.addEventListener("scroll", changeHeader);
+
+  // Scroll to a tergeted section
+  const scrollToSectionButton = document.querySelectorAll(
+    "[href='#calendlyArea']"
+  );
+  const targetSection = document.getElementById("calendlyArea");
+  scrollToSectionButton.forEach((each) => {
+    each.addEventListener("click", (e) => {
+      e.preventDefault();
+      targetSection.scrollIntoView({ behavior: "smooth" });
+    });
+  });
+}
+
+export default pageTwicks;
