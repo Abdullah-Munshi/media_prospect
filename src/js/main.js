346a0f16c4e03484ef72d558a9e53a776474ff13
Abdullah Munshi <stackbind@gmail.com>
2023-10-15 17:08:42 +0600

    desktop part almost done

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,7 @@
-import videojs from "video.js";
 import Plyr from "plyr";
-import "../../node_modules/plyr/dist/plyr.css";
-
-// Calculate Hero video height base on screen height
-const heroVideoHeight =
-  (window.innerHeight || document.documentElement.clientHeight) - 270;
-
-// let player2 = videojs("#video-frame-2", {
-//   fluid: true,
-// });
-// let player3 = videojs("#video-frame-3", {
-//   fluid: true,
-// });
 
 const heroPlayer = new Plyr("#heroPlayer", {
-  // ratio: "16:9",
+  ratio: "16:9",
 });
 
 const players = Array.from(document.querySelectorAll(".video_player")).map(
