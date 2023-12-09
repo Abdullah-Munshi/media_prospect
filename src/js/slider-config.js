5fc6f6be1b21685760285cec4070c91d08877513
Abdullah Munshi <stackbind@gmail.com>
2023-12-09 23:37:35 +0600

    carousel added in testimonial section and top video changed with vimeo link except one video in testimonial carousel

--- src/js/slider-config.js
+++ src/js/slider-config.js
@@ -1,14 +1,13 @@
 import Splide from "@splidejs/splide";
 import { ytPlayersForSlider } from "./videoPlayer";
 function setupSlider() {
-  const exampleMain = new Splide(".example-slider", {
+  const mainSliderObj = {
     type: "fade",
     pagination: false,
     arrows: false,
     cover: true,
-  });
-
-  const exampleThumbnails = new Splide(".example-slider-thumbnail", {
+  };
+  const thumbSliderObj = {
     rewind: true,
     fixedWidth: 104,
     fixedHeight: 58,
@@ -29,11 +28,30 @@ function setupSlider() {
         fixedHeight: 38,
       },
     },
-  });
+  };
+  const exampleMain = new Splide(".example-slider", mainSliderObj);
+
+  const exampleThumbnails = new Splide(
+    ".example-slider-thumbnail",
+    thumbSliderObj
+  );
   exampleMain.sync(exampleThumbnails);
   exampleMain.mount();
   exampleThumbnails.mount();
   exampleMain.on("move", () => {
+    ytPlayersForSlider.forEach((ytPlayer) => ytPlayer.pause());
+  });
+
+  const testimonialMain = new Splide(".testimonial-slider", mainSliderObj);
+  const testimonialThumbnails = new Splide(
+    ".testimonial-slider-thumbnail",
+    thumbSliderObj
+  );
+
+  testimonialMain.sync(testimonialThumbnails);
+  testimonialMain.mount();
+  testimonialThumbnails.mount();
+  testimonialMain.on("move", () => {
     ytPlayersForSlider.forEach((ytPlayer) => ytPlayer.pause());
   });
 }
